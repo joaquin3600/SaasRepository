a2a2170a43d11ff92d64300fec45d432adadeef7
joaquin3600 <joaquin-dz@hotmail.com>
2024-01-22 23:45:13 -0600

    Creacion de Proyecto Web

---
+++ Proyectos/BL/Entity/CompanyAddress.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BL.Entity
+{
+    [Table("CompanyAddress")]
+
+    public class CompanyAddress
+    {
+    }
+}
---
+++ Proyectos/BL/Entity/Currency.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BL.Entity
+{
+    [Table("Currency")]
+    public class Currency
+    {
+        public long     ProductId { get; set; }
+        public string   Name { get; set; } = string.Empty;
+        [Column(TypeName = "decimal(8, 2)")]
+        public decimal  Price { get; set; }
+        public long     CategoryId { get; set; }
+        public Category? Category { get; set; }
+        public long SupplierId { get; set; }
+        public Supplier? Supplier { get; set; }
+
+    }
+}
